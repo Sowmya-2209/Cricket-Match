82a46f3035df476b0cbe720f45d3cbc6cf6f57f3
Sowmya <74712506+Sowmya-2209@users.noreply.github.com>
2020-11-22 13:50:32 +0530

    Add files via upload

---
+++ design.js
@@ -0,0 +1,141 @@
+
+
+//alert("hey everyone")
+// var ---->  global scope
+// let ----> block scope
+
+/*var = 10;
+let b ="name"
+const c =true
+
+console.log(a)
+console.log(b)
+console.log(c)
+
+console.log("data type of a = " + typeof(a))
+console.log("data type of b = " + typeof(b))
+console.log("data type of c = " + typeof(c))*/
+
+//Team1 Details
+var team1={
+    name:"CSK",
+    score:0,
+    runs:[]
+};
+
+//Team2 Details
+var team2={
+    name: "MI",
+    score:0,
+    runs:[]
+};
+
+var score=[0,1,2,3,4,5,6]
+
+console.log(team1)
+console.log(team2)
+
+var toss;
+
+window.onload=()=>{
+    selectToss();
+    updateName();
+    updateButtontext();
+    updateScores();
+
+}
+
+function selectToss(){
+    toss = Math.round(Math.random())+1
+    console.log(toss)
+}
+
+function updateName(){
+    console.log("team1 name : " + team1.name)
+    console.log("team2 name : " + team2.name)
+    document.getElementById("team1name").textContent=team1.name;
+    document.getElementById("team2name").textContent=team2.name;
+}
+
+function updateButtontext(){
+  /* if (toss === 1){
+       console.log("team1"+team1.name + "batting")
+   }
+   else{
+       console.log("team2"+team2.name + "batting")
+   }
+   */
+   
+    // console.log(toss===1?team1.name:team2.name + "Batting");
+var button = document.getElementById("strikebutton")
+var result = document.getElementById("result")
+result.style.visibility=""
+updateRuns();
+
+    //check if the game is over
+    if (team1.runs.length == 6 && team2.runs.length==6){
+        button.remove();
+
+       // console.log("both team finished their chances");
+       console.log(team1.score === team2.score ? "it is a draw" : `${team1.score > team2.score ? team1.name: team2.name} WINS`)
+    result.textContent = team1.score === team2.score ? "it is a draw" : `${team1.score > team2.score ? team1.name: team2.name} WINS`     
+    }
+    else{
+        //Strike over
+        toss=
+        team1.runs.length === 6 ? 2 :
+        team2.runs.length === 6 ? 1 :
+        toss;
+    }
+    console.log(`${toss===1?team1.name:team2.name} Batting`)
+    document.getElementById("strikebutton").textContent = `${toss === 1? team1.name : team2.name} Batting`;
+
+}
+
+handlestrike=()=>{
+    console.log("onclick works")
+    var runs =score[Math.floor(Math.random()*score.length)]
+    console.log(runs)
+    if(toss===1){
+        team1.runs.push(runs);
+        console.log(runs)
+        team1.score = calculatescore(team1.runs)
+    }
+    else{
+        team2.runs.push(runs);
+        console.log(runs)
+        team2.score = calculatescore(team2.runs)
+    }
+    updateButtontext();
+    updateScores();
+    
+}
+
+function updateScores(){
+    console.log("team1 score = " + team1.score)
+    console.log("team2 score = " + team2.score)
+    document.getElementById("team1score").textContent = team1.score;
+    document.getElementById("team2score").textContent = team2.score;
+    updateRuns();
+
+}
+
+function updateRuns(){
+    var team1runs = document.getElementById("team1runs").children;
+    var team2runs = document.getElementById("team2runs").children;
+
+    team1.runs.forEach((run, index)=>{
+        team1runs[index].textContent =run;
+    });
+
+    team2.runs.forEach((run,index)=>{
+        team2runs[index].textContent =run;
+    });
+
+}
+
+var calculatescore=(runs)=>{
+    return runs.map(num=>{
+        return num;
+    }).reduce((total,num)=>total+num);
+}
